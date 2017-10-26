71b27838d99129fdfdfd72b720e0bbaa17c60783
Iulian Tabara <itabara@live.com>
2017-10-26 23:42:50 +0300

    Added remider

--- api/controllers/absences-controller.js
+++ api/controllers/absences-controller.js
@@ -5,6 +5,8 @@ exports.absencesPOST = function(args, res, next) {
   var email_address = args.body.email_address;
   var days_off = args.body.days_off;
 
+  // todo: add each day_off into database
+  
   var response = { message: `${days_off.length} days off added for: ${email_address}` };
   res.writeHead(200, { "Content-Type": "application/json" });
   return res.end(JSON.stringify(response));
